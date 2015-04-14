370094341b2b30ecd8fcb1a32e103be60cee362a
johnsonlee <g.johnsonlee@gmail.com>
2015-04-14 18:09:09 +0800

    Add log header for more information

--- main/src/com/beastbikes/framework/android/ApplicationContext.java
+++ main/src/com/beastbikes/framework/android/ApplicationContext.java
@@ -11,6 +11,7 @@ import android.app.Application.ActivityLifecycleCallbacks;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.ViewConfiguration;
 
@@ -25,11 +26,12 @@ import com.beastbikes.framework.android.runtime.DefaultUncaughtExceptionHandler;
 public abstract class ApplicationContext extends Application implements
 		ActivityLifecycleCallbacks {
 
-    static {
-        System.loadLibrary("trace");
-    }
+	static {
+		System.loadLibrary("trace");
+	}
 
-	private static final Logger logger = LoggerFactory.getLogger(ApplicationContext.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(ApplicationContext.class);
 
 	/**
 	 * Returns the value of the specified meta data
@@ -82,7 +84,31 @@ public abstract class ApplicationContext extends Application implements
 			logger.error(e.getMessage(), e);
 		}
 
-		logger.info("Application " + getPackageName() + " started");
+		logger.info(getLineSeparator(80, getPackageName()));
+		logger.info("> ro.bootloader                : " + Build.BOOTLOADER);
+		logger.info("> ro.build.id                  : " + Build.ID);
+		logger.info("> ro.build.display.id          : " + Build.DISPLAY);
+		logger.info("> ro.build.version.incremental : " + Build.VERSION.INCREMENTAL);
+		logger.info("> ro.build.version.release     : " + Build.VERSION.RELEASE);
+		logger.info("> ro.build.version.sdk         : " + Build.VERSION.SDK_INT);
+		logger.info("> ro.build.version.codename    : " + Build.VERSION.CODENAME);
+		logger.info("> ro.build.type                : " + Build.TYPE);
+		logger.info("> ro.build.tags                : " + Build.TAGS);
+		logger.info("> ro.build.fingerprint         : " + Build.FINGERPRINT);
+		logger.info("> ro.build.date.utc            : " + Build.TIME);
+		logger.info("> ro.build.user                : " + Build.USER);
+		logger.info("> ro.build.host                : " + Build.HOST);
+		logger.info("> ro.hardware                  : " + Build.HARDWARE);
+		logger.info("> ro.product.board             : " + Build.BOARD);
+		logger.info("> ro.product.brand             : " + Build.BRAND);
+		logger.info("> ro.product.cpu.abi           : " + Build.CPU_ABI);
+		logger.info("> ro.product.cpu.abi2          : " + Build.CPU_ABI2);
+		logger.info("> ro.product.device            : " + Build.DEVICE);
+		logger.info("> ro.product.manufacturer      : " + Build.MANUFACTURER);
+		logger.info("> ro.product.model             : " + Build.MODEL);
+		logger.info("> ro.product.name              : " + Build.PRODUCT);
+		logger.info("> ro.serialno                  : " + Build.SERIAL);
+		logger.info("================================================================================");
 	}
 
 	@Override
@@ -113,4 +139,21 @@ public abstract class ApplicationContext extends Application implements
 	public void onActivityDestroyed(Activity activity) {
 	}
 
+	private static String getLineSeparator(int width, String title) {
+		final int w = Math.max(0, width - 2);
+		final int l = (w - title.length()) / 2;
+		final int r = (w - title.length() - l);
+		final StringBuilder seprator = new StringBuilder();
+
+		for (int i = 0; i < l; i++) {
+			seprator.append('=');
+		}
+
+		seprator.append(' ').append(title).append(' ');
+
+		for (int i = 0; i < r; i++) {
+			seprator.append('=');
+		}
+		return seprator.toString();
+	}
 }
